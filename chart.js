f426d19e120a5c18a065aea225c1160c15a2f8be
Kenbo0601 <kenichi2@pdx.edu>
2020-07-16 22:07:35 -0700

    files modified

--- chart.js
+++ chart.js
@@ -1,12 +1,12 @@
 var logIn = document.getElementById("logIn");
-var chart = document.getElementById("chart");
+var home = document.getElementById("home");
 
 //jump to logIn.html
 logIn.onclick = function () {
   window.location.assign("logIn.html");
 };
 
-//jump to chart.html
-chart.onclick = function () {
-  window.location.assign("chart.html");
+//jump to index.html
+home.onclick = function () {
+  window.location.assign("index.html");
 };
--- logIn.js
+++ logIn.js
@@ -1,12 +1,12 @@
-var logIn = document.getElementById("logIn");
 var chart = document.getElementById("chart");
-
-//jump to logIn.html
-logIn.onclick = function () {
-  window.location.assign("logIn.html");
-};
+var home = document.getElementById("home");
 
 //jump to chart.html
 chart.onclick = function () {
   window.location.assign("chart.html");
+};
+
+//jump to index.html
+home.onclick = function () {
+  window.location.assign("index.html");
 };
