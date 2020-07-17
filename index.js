e8491c88e677870cf807c5ce2e1eb368ad649528
Kenbo0601 <kenichi2@pdx.edu>
2020-07-16 21:51:36 -0700

    filed modifed

---
+++ index.js
@@ -0,0 +1,12 @@
+var logIn = document.getElementById("logIn");
+var chart = document.getElementById("chart");
+
+logIn.onclick = function () {
+  console.log("logIn pressed");
+  window.location.assign("logIn.html");
+};
+
+logIn.onclick = function () {
+  console.log("logIn pressed");
+  window.location.assign("chart.html");
+};
