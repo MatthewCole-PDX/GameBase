02c76d30cfd5ca34756d60ada73d9207ec0ac858
MatthewCole-PDX <macole@pdx.edu>
2020-08-02 20:49:22 -0700

    home to /

--- src/main.js
+++ src/main.js
@@ -22,7 +22,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use('/home', indexRouter);
+app.use('/', indexRouter);
 
 module.exports = app;
 //end
