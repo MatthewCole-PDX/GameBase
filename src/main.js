9194f6a8d476b7c644305701d1e23ed118b82b06
MatthewCole-PDX <macole@pdx.edu>
2020-08-04 13:08:41 -0700

    created addNewUser

--- src/main.js
+++ src/main.js
@@ -12,6 +12,7 @@ var parser = require('body-parser');
 const PORT = process.env.PORT || 5000;
 // pool controls connections to the postgres db
 const { Client } = require('pg');
+const { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } = require("constants");
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl: {
@@ -28,40 +29,62 @@ app.use(
   })
 );
 
+var loggedIn = false;
+
 // load all of the files
 app.use(express.static(path.join(__dirname + "/public")));
 
 app.get("/", (req, res) => {
   res.status(200);
+  if(!loggedIn){
   res.sendFile(path.join(__dirname + "/public/index.html"));
+  }
+  else{
+    //send logged in version of index.html
+  }
 });
 
 // add possibility for manual navigation
 app.get("/chart", (req, res) => {
   res.status(200);
-  res.sendFile(path.join(__dirname + "/public/chart.html"));
+  if(!loggedIn){
+    res.sendFile(path.join(__dirname + "/public/chart.html"));
+  }
+  else{
+    //send logged in version of chart.html
+  }
 });
 
 app.get("/login", (req, res) => {
   res.status(200);
   res.sendFile(path.join(__dirname + "/public/logIn.html"));
+  //not visible if logged in
+});
+
+app.get("/newUserAdded", (req, res) => {
+  res.status(200);
+  client.connect();
+  client.query('INSERT INTO users(user_name, password, email) VALUES (req.body.name, req.body.password, req.body.email);');
+    if (err) throw err;
+  res.sendFile(path.join(__dirname + "/public/newUserAdded.html"));
 });
 
 app.post("/search", (req, res) => {
   res.status(200);
-  res.send("Searching for " + req.body.searchFor + " in " + req.body.category);
+  var search = ("Searching for " + req.body.searchFor + " in " + req.body.category);
   //res.sendFile(path.join(__dirname + "/public/search.html"));
-  /*
+  
   client.connect();
   client.query('SELECT name FROM ' + req.body.category + 'WHERE name LIKE ' + req.body.searchFor +';', (err, res) => {
     if (err) throw err;
-    res.set({"Content-Type": "text/plain"});
+    res.set({"Content-Type": "text/html"});
     for (let row of res.rows) {
-      res.write(JSON.stringify(row));
+      search += JSON.stringify(row);
+      search += '</br>';
     }
     client.end();
+    res.send(search);
   });
-  */
   
 });
 
